ca030283bf77a36f0087a5d2b8f029a6be65a840
Oliver Personal Laptop <oliverblauritsen@gmail.com>
2026-09-20 21:32:44 +0200

    auth: merge get_user and require_auth
    get_user now returns the user object, and takes a "required" parameter,
    raise HTTPException if required is True and no user session is found.

--- backend/app/authentication/routes.py
+++ backend/app/authentication/routes.py
@@ -1,23 +1,16 @@
 """HTTP routes for reporting and managing authentication state."""
 
-import uuid
-
 from fastapi import APIRouter, Depends
 
-from sqlalchemy.orm import Session as DBSession
-
-from .auth import require_auth
+from .auth import get_user
 from .models import User
-from ..database import get_db
 
 router = APIRouter(tags=["auth"])
 
 
 @router.get("/user")
-def user_endpoint(user_id: uuid.UUID = Depends(require_auth), db: DBSession = Depends(get_db)):
+def user_endpoint(user: User = Depends(get_user())):
     """Return the authenticated user's public account identity."""
-    user = db.get(User, user_id)
-
     return {"id": user.id, "email": user.email}
 
 
